72ce5e7c4182a7c6d0a49d964f7d76587aa1c1c1
linsuiyuan <linsuiyuan@icloud.com>
2024-10-25 13:47:34 +0800

    实现 ItemLoader 半规则化提取

--- scrapy_universal_demo/items.py
+++ scrapy_universal_demo/items.py
@@ -1,12 +1,19 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
-import scrapy
+from itemloaders.processors import TakeFirst, Identity, Compose  # noqa
+from scrapy import Field, Item
+from scrapy.loader import ItemLoader
 
 
-class ScrapyUniversalDemoItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class MovieItem(Item):
+    name = Field()
+    cover = Field()
+    categories = Field()
+    published_at = Field()
+    drama = Field()
+    score = Field()
+
+
+class MovieItemLoader(ItemLoader):
+    default_output_processor = TakeFirst()
+    categories_out = Identity()
+    score_out = Compose(TakeFirst(), str.strip)
+    drama_out = Compose(TakeFirst(), str.strip)
--- scrapy_universal_demo/spiders/movie.py
+++ scrapy_universal_demo/spiders/movie.py
@@ -2,6 +2,8 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.http import Response
 
+from scrapy_universal_demo.items import MovieItem, MovieItemLoader
+
 
 class MovieSpider(CrawlSpider):
     name = "movie"
@@ -16,4 +18,12 @@ class MovieSpider(CrawlSpider):
     )
 
     def parse_detail(self, response: Response):
-        print(response.url)
+        item = MovieItem()
+        loader = MovieItemLoader(item=item, response=response)
+        loader.add_css("name", ".item h2::text")
+        loader.add_css("cover", ".cover::attr(src)")
+        loader.add_css("categories", ".categories .category span::text")
+        loader.add_css("published_at", ".info span::text", re=r"(\d{4}-\d{2}-\d{2})\s?上映")
+        loader.add_css("drama", ".drama p::text")
+        loader.add_css("score", "p.score::text")
+        yield loader.load_item()
